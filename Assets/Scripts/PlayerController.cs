63fed92975c1a5a73223c6632fda0bf4ee583e9d
Phrog84 <100796575+Phrog84@users.noreply.github.com>
2022-09-12 15:59:40 -0700

    Added Ship Tilt
    Ship tilt with buttons

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,16 +7,41 @@ public class PlayerController : MonoBehaviour
     private string hor = "Horizontal";
     private string ver = "Vertical";
 
+    //private Rigidbody rb;
+
+    public GameObject spaceShip;
+    private Vector3 center;
+
     public float speed = 8f;
+<<<<<<< Updated upstream
     /*public float Speed
     {
         get { return Speed; }
         set { Speed = value; }
     }*/
+=======
+    public float tiltDirection = 45f;
+>>>>>>> Stashed changes
 
     private float _xClamp = 9;
     private float _yClamp = 5;
 
+<<<<<<< Updated upstream
+=======
+    public GameObject playerLaserPrefab;
+    public GameObject playerLaserOrigin;
+    public int poolNumber;
+
+    public float canFire = -.5f;
+    public float fireRate = .5f;
+
+    private void Start()
+    {
+        spawnPooler = SpawnPooler.spawnPooler;
+        // rb = GetComponent<Rigidbody>();
+    }
+
+>>>>>>> Stashed changes
     private void FixedUpdate()
     {
         MoveSpaceShip();
@@ -28,8 +53,25 @@ public class PlayerController : MonoBehaviour
         float verticalInput = Input.GetAxisRaw(ver);
 
         Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+        Quaternion shipCenter = Quaternion.Euler(0, 0, 0);
+        Quaternion shipTiltRight = Quaternion.Euler(0, 0, tiltDirection);
+        Quaternion shipTiltLeft = Quaternion.Euler(0, 0, -tiltDirection);
 
-        transform.Translate(direction * speed * Time.fixedDeltaTime);
+        transform.Translate(speed * Time.deltaTime * direction);
+        // spaceShip.transform.rotation = Quaternion.Slerp(shipCenter, (shipTiltRight * horizontalInput), 1));
+
+        if (horizontalInput == 1)
+        {
+            spaceShip.transform.rotation = Quaternion.Lerp(shipCenter, shipTiltRight, 1);
+        }
+        else if (horizontalInput == -1)
+        {
+            spaceShip.transform.rotation = Quaternion.Slerp(shipCenter, shipTiltLeft, 1);
+        }
+        else
+        {
+            spaceShip.transform.rotation = shipCenter;
+        }
 
         transform.position = new Vector3(Mathf.Clamp(transform.position.x, -_xClamp, _xClamp), Mathf.Clamp(transform.position.y, -_yClamp, _yClamp), 0);
     }
